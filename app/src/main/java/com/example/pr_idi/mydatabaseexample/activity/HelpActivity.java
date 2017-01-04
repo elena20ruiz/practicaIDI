87bc84925c45ad1e540a437081e9359393e1066e
elena20ruiz <elena20ruiz@github.com>
2017-01-04 17:04:30 +0100

    Afegit Navigation Drawer + Floatting button per afegir i treure pelicules

---
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/activity/HelpActivity.java
@@ -0,0 +1,27 @@
+package com.example.pr_idi.mydatabaseexample.activity;
+
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pr_idi.mydatabaseexample.R;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
